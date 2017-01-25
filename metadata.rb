d5d846f4f483d3e21be856010d1feaad076aeccd
Rudger Gravestein <rudgergravestein@gmail.com>
2017-01-25 14:30:23 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache License, Version 2.0'
 description 'Installs/Configures vsftpd for Secure SSL SFTP'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.2.1'
 
 %w{ debian ubuntu }.each do |os|
   supports os
